f7e3d1440b84e6342488734f8db7ac08f5ae8936
Slawa <spidgorny@gmail.com>
2020-12-12 13:42:51 +0000

    feat: enable controlling specific motors

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import os
+import time
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -11,11 +13,27 @@ def hello_world():
 
 @app.route('/cola/<int:seconds>')
 def cola(seconds):
-    # run motor 2 for 5 seconds
+    # run motor  for 5 seconds
+    motorOn(1, seconds)
     return 'Cola for ' + str(seconds) + ' ready'
 
 
 @app.route('/fanta/<int:seconds>')
 def fanta(seconds):
     # run motor 2 for 5 seconds
+    motorOn(2, seconds)
     return 'Fanta ready for ' + str(seconds)
+
+@app.route('/sprite/<int:seconds>')
+def sprite(seconds):
+    # run motor 3 for 5 seconds
+    motorOn(3, seconds)
+    return 'Fanta ready for ' + str(seconds)
+
+def motorOn(motorID, seconds):
+    os.system('uhubctl/uhubctl -l 1-1.2 -p ' + str(motorID) + ' -a 1')
+    time.sleep(seconds)
+    os.system('uhubctl/uhubctl -l 1-1.2 -p ' + str(motorID) + ' -a 1')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
